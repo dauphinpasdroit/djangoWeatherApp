ea1fb9ce61877b29a959265a6ef474fde97ce29e
thomas <thomasmarty94@hotmail.fr>
2024-03-03 00:58:44 +0100

    django files

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    API_KEY = open("API_KEY", "r").read()
+    API_KEY = "21c58a2e0167908e6159300802e984ef"
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecat_url = "https://api.openweathermap.org/data/2.5/onecall?lat{}&long={}&exclude=current,minutely,hourly,alerts&appid={}"
+    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat{}&long={}&exclude=current,minutely,hourly,alerts&appid={}"
 
-    if request.methode == "POST" :
+    if request.method == "POST" :
         city1 = request.POST['city1'] 
-        city2 = request.get('city2', None)
+        city2 = request.POST.get('city2', None)
 
         weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
 
@@ -30,25 +30,25 @@ def index(request):
         return render(request, "weather_app/index.html")
 
 def fetch_weather_and_forecast(city, API_KEY, current_weather_url, forecast_url):
-    response = requests.get(current_weather_url.format(city, api_key)).json()
-    lat, long = response['coord']['lat'], response['coord']['long'] 
-    forecat_response = requests.get(forecast_url.format(lat, lon, API_KEY)).json()
+    response = requests.get(current_weather_url.format(city, API_KEY)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon'] 
+    forecast_response = requests.get(forecast_url.format(lat, lon, API_KEY)).json()
 
     weather_data = {
         "city": city,
-        "temperature": round(response['main']['temperature'] - 274.15, 2),
-        "description": response['weather'][O]['description']   
-        "icon": response['weather'][0]['icon']
+        "temperature": round(response['main']['temp'] - 274.15, 2),
+        "description": response['weather'][0]['description']   ,
+        "icon": response['weather'][0]['icon'],
     }      
 
     daily_forecasts = []
     for daily_data in forecast_response['daily'][:5]:
         daily_forcasts.append({
             "day": daytime.datetime.fromtimestampe(daily_data['dt']).strfrtime("%A"),
-            "min_temp": round(daily_data['temp']['min'] - 274.15, 2)
-            "max_temp": round(daily_data['temp']['max'] - 274.15, 2)
+            "min_temp": round(daily_data['temp']['min'] - 274.15, 2),
+            "max_temp": round(daily_data['temp']['max'] - 274.15, 2),
             "description": daily_data['weather'][0]['description'],
-            "icon": daily_data['weather'][0]['icon']
+            "icon": daily_data['weather'][0]['icon'],
         })
 
     return weather_data, daily_forecasts
